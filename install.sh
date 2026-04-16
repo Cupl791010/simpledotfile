42cde2372f130f7aa0bc59580cf4692847ac4c95
Cupl791010 <110648413+Cupl791010@users.noreply.github.com>
2026-04-16 15:55:15 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+
+
+echo "Installing... >> simple dotfiles"
+
+./install/install_packages.sh
+./install/load_symlinks.sh
+
+
+
+echo "......................................................"
+echo "SUCCESSFULL!!"
+
+
+
+
+
+
+
+
+
+
