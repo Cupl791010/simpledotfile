e96693e5b8e7236c2e9f1d64548510fbff4d1049
Cupl791010 <110648413+Cupl791010@users.noreply.github.com>
2026-04-16 15:36:31 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+
+
+echo "Installing... >> niii-san dotfiles"
+
+./.install/install_packages.sh
+./.install/load_symlinks.sh
+
+
+
+echo "......................................................"
+echo "SUCCESSFULL!!"
+
+
+
+
+
+
+
+
+
+
